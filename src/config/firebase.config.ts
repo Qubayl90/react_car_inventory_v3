26413271c781feafc147f26793e4bcb5650d1b6d
Qubayl90 <Qubayl90@gmail.com>
2023-05-07 13:20:02 +0530

    Add files via upload

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqF5JcpPRVlyzncyzLW-UfOjMF7an-mHY",
+  authDomain: "sp-fs-react-practice.firebaseapp.com",
+  projectId: "sp-fs-react-practice",
+  storageBucket: "sp-fs-react-practice.appspot.com",
+  messagingSenderId: "1064762516882",
+  appId: "1:1064762516882:web:127dd00ec7bd59a0fd0d4b"
+};
+
